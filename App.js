04005cea834a0499c1b8e838d28591263df70b12
Kevin Goble <goble.kevin@gmail.com>
2023-11-30 14:21:35 -0800

    Adds computer ability for random choice

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TurboModuleRegistry, View } from 'react-native';
 
 import { useState } from 'react';
+
+const options = ["Rock", "Paper", "Scissors"];
+
+function getRandomArrayElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
 
 export default function App() {
   const [playerChoice, setPlayerChoice] = useState(null);
   const [computerChoice, setComputerChoice] = useState(null);
+  
 
   const makeMoves = (choice) => {
-    setComputerChoice("Rock");
+    setComputerChoice(getRandomArrayElement(options));
     setPlayerChoice(choice);
   }
 
+  const isPlayerWinner = () => {
+    //rock beats scissors
+    //scissors beats paper
+    //paper beats rock
+    let result = false;
+    if (playerChoice == "Rock" && computerChoice == "Scissors") {
+      result = true;
+    }
+    if (playerChoice == "Paper" && computerChoice == "Rock") {
+      result = true;
+    }
+    if (playerChoice == "Scissors" && computerChoice == "Paper") {
+      result = true;
+    }
+    return result;
+  }
 
 
   return (
